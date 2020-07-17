6a78e4443e0813907d671f58d77409e67604df46
mailtime <mailtime2010@gmail.com>
2020-07-17 22:59:23 +0300

    circles life of component

--- theoryapp/src/app/flies/flies.component.ts
+++ theoryapp/src/app/flies/flies.component.ts
@@ -10,18 +10,26 @@ export class FliesComponent {
   flies = [{
     name: 'Ford',
     year: 2020
-  }, {
+  }/*, {
     name: 'BMW',
     year: 2022
   }, {
     name: 'Mercedes',
     year: 2021
-  }];
+  }*/];
 
   constructor() {
   }
 
   addFliesByEvent(fly: { name: string, year: number }) {
     this.flies.push(fly);
+  }
+
+  changeCarName() {
+    this.flies[0].name = 'new car name';
+  }
+
+  deleteCar() {
+    this.flies.splice(0, 1);
   }
 }
--- theoryapp/src/app/fly/fly.component.ts
+++ theoryapp/src/app/fly/fly.component.ts
@@ -1,15 +1,68 @@
-import {Component, ContentChild, ElementRef, Input} from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  DoCheck,
+  ElementRef,
+  Input,
+  OnChanges, OnDestroy,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
 
 @Component({
   selector: 'app-fly',
   templateUrl: './fly.component.html',
   styleUrls: ['./fly.component.css']
 })
-export class FlyComponent {
+export class FlyComponent implements OnInit,
+  OnChanges, DoCheck, AfterContentInit, AfterContentChecked,
+  AfterViewInit, AfterViewChecked, OnDestroy{
   @Input('flyItem') fly: { name: string, year: number; };
   @ContentChild('outputRef') outputRef: ElementRef;
+  @Input() name: string;
 
   ngAfterViewInit() {
     console.log(this.outputRef);
+  }
+
+  /*life circles components*/
+  /*first level callback*/
+
+  /*create all each*/
+  constructor() {
+    console.log('constructor');
+  }
+
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 }
