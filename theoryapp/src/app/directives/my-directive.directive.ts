260471467c41b43a2bb46bef7dd728687e61c9df
mailtime <mailtime2010@gmail.com>
2020-07-18 22:34:58 +0300

    directive renderer setStyle addClass nativeElement

--- theoryapp/src/app/directives/my-directive.directive.ts
+++ theoryapp/src/app/directives/my-directive.directive.ts
@@ -1,16 +1,19 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appMyDirective]'
 })
-export class MyDirectiveDirective implements OnInit{
+export class MyDirectiveDirective implements OnInit {
 
-  constructor(private element: ElementRef) {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
     console.log(this.element);
-    this.element.nativeElement.style.backgroundColor = 'red';
+    const {nativeElement} = this.element;
+    this.renderer.setStyle(nativeElement, 'background-color', 'blue');
+    this.renderer.addClass(nativeElement, 'white-text');
+    /*this.element.nativeElement.style.backgroundColor = 'red';*/
   }
 
 }
