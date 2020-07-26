2d19b52a97d366c380094905d52114cb19a1bf6e
mailtime <mailtime2010@gmail.com>
2020-07-26 21:12:05 +0300

    server HttpHeader  8

--- theoryapp/src/app/server/cars.service.ts
+++ theoryapp/src/app/server/cars.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable()
 export class CarsService {
@@ -8,7 +8,8 @@ export class CarsService {
   }
 
   getCarsByGET() {
-    return this.http.get('http://localhost:3000/cars');
+    const headers: HttpHeaders  = new HttpHeaders ({'Content-Type': 'application/json; charset=utf8'});
+    return this.http.get('http://localhost:3000/cars', {headers});
   }
 
   addCarByPOST(carName: string) {
@@ -16,12 +17,13 @@ export class CarsService {
       name: carName,
       color: 'blue'
     }
-    return this.http.post('http://localhost:3000/cars', data);
+    const headers = new HttpHeaders ({'Content-Type': 'application/json; charset=utf8'});
+    return this.http.post('http://localhost:3000/cars', data, {headers});
   }
 
   changeColor(car: any, color: string) {
     car.color = color;
-    return this.http.put(`http://localhost:3000/cars/${car.id}`, {name:car.name, color});
+    return this.http.put(`http://localhost:3000/cars/${car.id}`, {name: car.name, color});
   }
 
   deleteCar(car: any) {
